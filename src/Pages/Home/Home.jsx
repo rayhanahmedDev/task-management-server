37c98f27f358d183438c535da8905c0b289aa7d4
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-12-21 15:00:49 +0600

    build footer section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../Shared/Footer/Footer";
 import NavBer from "../Shared/NavBer/NavBer";
 import Banner from "./Banner/Banner";
 import Benefit from "./Benifit/Benefit";
@@ -12,6 +13,9 @@ const Home = () => {
             </div>
             <div className="mt-20">
                 <Benefit></Benefit>
+                <div className="mt-20">
+                    <Footer></Footer>
+                </div>
             </div>
         </div>
     );
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import { FaFacebook, FaLinkedinIn, FaSkype, } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <div>
+            {/* footer start */}
+            <footer className="footer footer-center p-10 rounded bg-gradient-to-r from-[#D6FFFC] to-[#ffeff5] text-black">
+                <nav>
+                    <div className="grid grid-flow-col gap-4">
+                        <Link to={`https://www.facebook.com/profile.php?id=100014010706300`}>
+                            <FaFacebook className='text-3xl hover:text-[#E60072]'></FaFacebook>
+                        </Link>
+                        <Link to={`https://www.linkedin.com/in/rayhanahmeddev`}>
+                            <FaLinkedinIn className='text-3xl hover:text-[#E60072]'></FaLinkedinIn>
+                        </Link>
+                        <Link to={`https://join.skype.com/invite/vd7eE5fW7yDf`}>
+                            <FaSkype className='text-3xl hover:text-[#E60072]'></FaSkype>
+                        </Link>
+                    </div>
+                </nav>
+                <aside>
+                    <p>Copyright © 2023 - All Right Reserved By Rayhan Ahmed</p>
+                </aside>
+            </footer>
+            {/* footer end */}
+        </div>
+    );
+};
+
+export default Footer;
